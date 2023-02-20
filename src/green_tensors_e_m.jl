e761c32df9f2f4debbb1bc216d9e28de21753822
augustinmuster <augustin@must-r.com>
2023-02-20 08:56:25 +0100

    Repair green_tensors

--- src/green_tensors_e_m.jl
+++ src/green_tensors_e_m.jl
@@ -33,15 +33,15 @@ function G_em(r1,r2,k)
     kR = norm(kR_vec)
     kR2 = kR^2
     Ur=kR_vec/kR
-    
+
     term1=exp(im*kR)/(4*pi*kR)*k
     term2=1+(im/(kR))-(1/(kR2))
     term3=1+(3*im/(kR))-(3/(kR2))
     term4=(im*kR-1)/kR
     matrix=kR_vec*transpose(kR_vec)/kR2
-    id3=[1 0 0;0 1 0;0 0 1]   
+    id3=[1 0 0;0 1 0;0 0 1]
     mat=[0 -Ur[3] Ur[2];Ur[3] 0 -Ur[1];-Ur[2] Ur[1] 0]
-    
+
     Ge = term1*(term2*id3-term3*matrix) 	#same as G_e(r1,r2,knorm)
     Gm = term1*term4*mat			#same as G_m(r1,r2,knorm)/knorm
 
@@ -68,15 +68,15 @@ function G_em_renorm(kr1,kr2)
     kR = norm(kR_vec)
     kR2 = kR^2
     Ur=kR_vec/kR
-    
+
     term1=exp(im*kR)/(kR)
     term2=1+(im/(kR))-(1/(kR2))
     term3=1+(3*im/(kR))-(3/(kR2))
     term4=(im*kR-1)/kR
     matrix=kR_vec*transpose(kR_vec)/kR2
-    id3=[1 0 0;0 1 0;0 0 1]   
+    id3=[1 0 0;0 1 0;0 0 1]
     mat=[0 -Ur[3] Ur[2];Ur[3] 0 -Ur[1];-Ur[2] Ur[1] 0]
-    
+
     Ge = term1*(term2*id3-term3*matrix) 	#same as G_e_renorm(kr1,kr2)
     Gm = term1*term4*mat			#same as G_m_renorm(kr1,kr2)
 
@@ -334,11 +334,11 @@ Compute the derivative green tensor (defined in G_e(r1,r2,knorm) and G_m(r1,r2,k
 The output are two 3x3 complex matrix (with units of [1/m^2]).
 """
 function dxG_em(r1,r2,knorm)
-    
+
     R_vec = r1-r2
     R = norm(R_vec)
     Ur=R_vec/R
-    
+
     x = R_vec[1]
     y = R_vec[2]
     z = R_vec[3]
@@ -352,7 +352,7 @@ function dxG_em(r1,r2,knorm)
     id3=[1 0 0;0 1 0;0 0 1]
 
     dxGe =  term1*(term2*id3 + term3*matrix + term4*Rmx)	# same as dxG_e(r1,r2,knorm)
-    
+
     #difference vector
 
     #create empty 3x3 matirx to store the green tensor
@@ -365,7 +365,7 @@ function dxG_em(r1,r2,knorm)
     term6=(im*knorm*R)^2 - 3*im*knorm*R + 3
     #return green tensor
     dxGm = term1/R^2*(term6*mat*Ur[1] + term5*mat3)/knorm		# same as dxG_m(r1,r2,knorm)/knorm
-    
+
     return dxGe, dxGm
 end
 
@@ -375,11 +375,11 @@ Compute the derivative green tensor (defined in G_e(r1,r2,knorm) and G_m(r1,r2,k
 The output are two 3x3 complex matrix (with units of [1/m^2]).
 """
 function dyG_em(r1,r2,knorm)
-    
+
     R_vec = r1-r2
     R = norm(R_vec)
     Ur=R_vec/R
-    
+
     x = R_vec[1]
     y = R_vec[2]
     z = R_vec[3]
@@ -393,7 +393,7 @@ function dyG_em(r1,r2,knorm)
     id3=[1 0 0;0 1 0;0 0 1]
 
     dyGe =  term1*(term2*id3 + term3*matrix + term4*Rmy)	# same as dxG_e(r1,r2,knorm)
-    
+
     #difference vector
 
     #create empty 3x3 matirx to store the green tensor
@@ -406,7 +406,7 @@ function dyG_em(r1,r2,knorm)
     term6=(im*knorm*R)^2 - 3*im*knorm*R + 3
     #return green tensor
     dyGm = term1/R^2*(term6*mat*Ur[2] + term5*mat3)/knorm		# same as dxG_m(r1,r2,knorm)/knorm
-    
+
     return dyGe, dyGm
 end
 
@@ -416,11 +416,11 @@ Compute the derivative green tensor (defined in G_e(r1,r2,knorm) and G_m(r1,r2,k
 The output are two 3x3 complex matrix (with units of [1/m^2]).
 """
 function dzG_em(r1,r2,knorm)
-    
+
     R_vec = r1-r2
     R = norm(R_vec)
     Ur=R_vec/R
-    
+
     x = R_vec[1]
     y = R_vec[2]
     z = R_vec[3]
@@ -434,7 +434,7 @@ function dzG_em(r1,r2,knorm)
     id3=[1 0 0;0 1 0;0 0 1]
 
     dzGe =  term1*(term2*id3 + term3*matrix + term4*Rmz)	# same as dxG_e(r1,r2,knorm)
-    
+
     #difference vector
 
     #create empty 3x3 matirx to store the green tensor
@@ -447,7 +447,7 @@ function dzG_em(r1,r2,knorm)
     term6=(im*knorm*R)^2 - 3*im*knorm*R + 3
     #return green tensor
     dzGm = term1/R^2*(term6*mat*Ur[3] + term5*mat3)/knorm		# same as dxG_m(r1,r2,knorm)/knorm
-    
+
     return dzGe, dzGm
 end
 
@@ -456,21 +456,132 @@ end
 Sigma represent the base-change-matrix [E, ZH] to [E, iZH]
 """
 function Sigma(n)
-    
+
     Sigma = zeros(ComplexF64,6*n,6*n)
     Sigma_i = zeros(ComplexF64,6*n,6)
     id = [1 0 0;0 1 0;0 0 1]
-    
-    for i = 1:n    
+
+    for i = 1:n
         Sigma_i[(i-1)*6+1:(i-1)*6+6,:] = [id id*0; id*0 im*id]
     end
-    for i = 1:n    
+    for i = 1:n
         Sigma[:,(i-1)*6+1:(i-1)*6+6] = copy(Sigma_i)
-    end    
+    end
     return Sigma
-    
+
 end
 
+
+@doc raw"""
+    G_e_m(r1,r2,knorm)
+Compute the big electric and magnetic green tensor between two position  `r1` and `r2` with wavenumber `knorm`.
+The output is a 6x6 complex matrix.
+The big electric and magnetic green tensor is defined as:
+```math
+ \tilde{G}_{em}=\left(\begin{matrix}
+ \tilde{G}_e &  \tilde{G}_m/k\\
+ \tilde{G}_m/k &  \tilde{G}_e
+\end{matrix}\right)
+```
+"""
+function G_e_m(r1,r2,knorm)
+    Gem=zeros(ComplexF64,6,6)
+    Gem[1:3,1:3]=G_e(r1,r2,knorm)
+    Gem[4:6,4:6]=G_e(r1,r2,knorm)
+    Gem[1:3,4:6]=G_m(r1,r2,knorm)/knorm
+    Gem[4:6,1:3]=G_m(r1,r2,knorm)/knorm
+    return Gem
+end
+
+
+@doc raw"""
+    G_e_m_renorm(r1,r2,knorm)
+Compute the big electric and magnetic green tensor in renormalized units (see Home page) between two position  `r1` and `r2` with wavenumber `knorm`.
+The output is a 6x6 complex matrix.
+The big electric and magnetic green tensor in renormalized units is defined as:
+```math
+ G_{em}=\left(\begin{matrix}
+ G_e &  G_m\\
+ G_m &  G_e
+\end{matrix}\right)
+```
+"""
+function G_e_m_renorm(r1,r2)
+    Gem=zeros(ComplexF64,6,6)
+    Gem[1:3,1:3]=G_e_renorm(r1,r2)
+    Gem[4:6,4:6]=G_e_renorm(r1,r2)
+    Gem[1:3,4:6]=G_m_renorm(r1,r2)
+    Gem[4:6,1:3]=G_m_renorm(r1,r2)
+    return Gem
+end
+
+function dxG_e_m_renorm(r1,r2,knorm)
+    Gem=zeros(ComplexF64,6,6)
+    Gem[1:3,1:3]=dxG_e(r1,r2,knorm)*4*pi/knorm
+    Gem[4:6,4:6]=dxG_e(r1,r2,knorm)*4*pi/knorm
+    Gem[1:3,4:6]=dxG_m(r1,r2,knorm)*4*pi/knorm^2
+    Gem[4:6,1:3]=dxG_m(r1,r2,knorm)*4*pi/knorm^2
+    return Gem
+end
+
+function dyG_e_m_renorm(r1,r2,knorm)
+    Gem=zeros(ComplexF64,6,6)
+    Gem[1:3,1:3]=dyG_e(r1,r2,knorm)*4*pi/knorm
+    Gem[4:6,4:6]=dyG_e(r1,r2,knorm)*4*pi/knorm
+    Gem[1:3,4:6]=dyG_m(r1,r2,knorm)*4*pi/knorm^2
+    Gem[4:6,1:3]=dyG_m(r1,r2,knorm)*4*pi/knorm^2
+    return Gem
+end
+
+function dzG_e_m_renorm(r1,r2,knorm)
+    Gem=zeros(ComplexF64,6,6)
+    Gem[1:3,1:3]=dzG_e(r1,r2,knorm)*4*pi/knorm
+    Gem[4:6,4:6]=dzG_e(r1,r2,knorm)*4*pi/knorm
+    Gem[1:3,4:6]=dzG_m(r1,r2,knorm)*4*pi/knorm^2
+    Gem[4:6,1:3]=dzG_m(r1,r2,knorm)*4*pi/knorm^2
+    return Gem
+end
+@doc raw"""
+    dxG_e_m(r1,r2,knorm)
+Compute the derivative of the big electric and magnetic green tensor regarding the x component of `r1` between two position  `r1` and `r2` with wavenumber `knorm`.
+The output isd a 6x6 complex matrix
+"""
+function dxG_e_m(r1,r2,knorm)
+    Gem=zeros(ComplexF64,6,6)
+    Gem[1:3,1:3]=dxG_e(r1,r2,knorm)
+    Gem[4:6,4:6]=dxG_e(r1,r2,knorm)
+    Gem[1:3,4:6]=dxG_m(r1,r2,knorm)/knorm
+    Gem[4:6,1:3]=dxG_m(r1,r2,knorm)/knorm
+    return Gem
+end
+
+@doc raw"""
+    dyG_e_m(r1,r2,knorm)
+Compute the derivative of the big electric and magnetic green tensor regarding the y component of `r1` between two position  `r1` and `r2` with wavenumber `knorm`.
+The output isd a 6x6 complex matrix
+"""
+function dyG_e_m(r1,r2,knorm)
+    Gem=zeros(ComplexF64,6,6)
+    Gem[1:3,1:3]=dyG_e(r1,r2,knorm)
+    Gem[4:6,4:6]=dyG_e(r1,r2,knorm)
+    Gem[1:3,4:6]=dyG_m(r1,r2,knorm)/knorm
+    Gem[4:6,1:3]=dyG_m(r1,r2,knorm)/knorm
+    return Gem
+end
+
+@doc raw"""
+    dzG_e_m(r1,r2,knorm)
+Compute the derivative of the big electric and magnetic green tensor regarding the z component of `r1` between two position  `r1` and `r2` with wavenumber `knorm`.
+The output isd a 6x6 complex matrix
+"""
+function dzG_e_m(r1,r2,knorm)
+    Gem=zeros(ComplexF64,6,6)
+    Gem[1:3,1:3]=dzG_e(r1,r2,knorm)
+    Gem[4:6,4:6]=dzG_e(r1,r2,knorm)
+    Gem[1:3,4:6]=dzG_m(r1,r2,knorm)/knorm
+    Gem[4:6,1:3]=dzG_m(r1,r2,knorm)/knorm
+    return Gem
+end
 
 
 if testing
