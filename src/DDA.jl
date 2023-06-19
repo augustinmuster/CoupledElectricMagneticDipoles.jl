fca891b5d04ad2738a0ca8b67454e152760344ea
augustinmuster <augustin@must-r.com>
2023-06-19 11:26:12 +0200

    Changes mostly to increase performances

--- src/DDA.jl
+++ src/DDA.jl
@@ -35,6 +35,7 @@ function solve_system(A,b,solver,verbose)
             println("solving with CPU LAPACK solver ...")
         end
         #
+        BLAS.set_num_threads(8)
         LAPACK.gesv!(A,phi)
         #
         return phi
@@ -73,7 +74,7 @@ function load_dda_matrix_e(kr,alpha_dl,verbose)
     A=Matrix{ComplexF64}(I,3*n,3*n)
     #if scalar polarisability
     if length(alpha_dl)==n
-        for j in 1:n
+        Threads.@threads for j in 1:n
             for k=1:j-1
                 G=GreenTensors.G_e_renorm(kr[j,:],kr[k,:])
                 A[3*(j-1)+1:3*(j-1)+3,3*(k-1)+1:3*(k-1)+3]=copy(-G*alpha_dl[k])
@@ -81,7 +82,7 @@ function load_dda_matrix_e(kr,alpha_dl,verbose)
             end
         end
     else #if tensor polarisabilty
-        for j in 1:n
+        Threads.@threads for j in 1:n
             for k=1:j-1
                 G=GreenTensors.G_e_renorm(kr[j,:],kr[k,:])
                 A[3*(j-1)+1:3*(j-1)+3,3*(k-1)+1:3*(k-1)+3]=copy(-G*alpha_dl[k,:,:])
@@ -117,7 +118,7 @@ function load_dda_matrix_e_m(kr,alpha_e_dl,alpha_m_dl,verbose)
     if length(alpha_e_dl)==n
         for i=1:n
             for j=1:i-1
-                Ge,Gm=GreenTensors.G_em_renorm(r[i,:],kr[j,:])
+                Ge,Gm=GreenTensors.G_em_renorm(kr[i,:],kr[j,:])
                 a_dda[1:3,1:3]=-Ge*alpha_e_dl[j]
                 a_dda[4:6,4:6]=-Ge*alpha_m_dl[j]
                 a_dda[1:3,4:6]=-im*Gm*alpha_m_dl[j]
--- src/geometries.jl
+++ src/geometries.jl
@@ -9,166 +9,89 @@ module Geometries
 ###########################
 using Base
 using LinearAlgebra
+using PyCall
+@pyimport matplotlib.pyplot as plt
 ###########################
 # FUNCTIONS
 ###########################
-@doc raw"""
-    gen_sphere_lattice_cubes(s,r;verbose=true)
-Discretizes a sphere of radius `R` in cubes on a cubic lattice where the radius of this sphere is divided in `s` lattice constants.
-`ref_id` is the refractive index id (we suppose that the entire sphere has the same refractive index.)
-Returns 2D array with first dimension is the `N` units that compose the sphere and the second is
-1) x-component of the position of the unit
-2) y-component of the position of the unit
-3) z-component of the position of the unit
-4) distance from the origin
-5) refractive index id
-6) lattice parameter
-7) volume of the cubic unit
-"""
-function gen_sphere_lattice_cubes(s,R;ref_id=1,verbose=true)
-    #logging
-    if verbose
-        println()
-        println("Generating a Sphere Lattice")
-        println()
-    end
-    #computing volume
-    V=(4/3)*pi*R^3
-    #printing
-    if verbose
-        println("you choose a sphere of radius ",R)
-        println("volume of the sphere:", V)
-        println("radius in units of lattice parameter=",s)
-    end
-    #counter
-    m=0
-    #iterate over all lattice state
-    for x=-(s+1):(s+1)
-        for y=-(s+1):(s+1)
-            for z=-(s+1):(s+1)
-                #positions in real dimensions
-                px=x
-                py=y
-                pz=z
-                #test if it is in the sphere
-                dis=sqrt(px^2+py^2+pz^2)
-                if dis<s
-                    #write if in the sphere
-                    m=m+1
+
+function discretize_sphere(a,N,N_sub)
+    #lattice parameter
+    dx=2*a/N
+    #sublattice parameter
+    dx_sub=dx/N_sub
+    #
+    center=[0.,0.,0.]
+    #
+    latt=[]
+    #
+    total_sub=N_sub^3
+    #
+    for i=1:N
+        xc=(i-0.5)*dx-a
+        for j=1:N
+            yc=(j-0.5)*dx-a
+            for k=1:N
+                zc=(k-0.5)*dx-a
+                inside=0
+                for n1=1:N_sub
+                    center[1]=(n1-0.5)*dx_sub+xc-dx/2
+                    for n2=1:N_sub
+                        center[2]=(n2-0.5)*dx_sub+yc-dx/2
+                        for n3=1:N_sub
+                            center[3]=(n3-0.5)*dx_sub+zc-dx/2
+                            if norm(center)<a
+                                inside+=1
+                            end
+                        end
+                    end
                 end
-            end
-        end
-    end
-    if verbose
-        println("numberof dipoles: ",m)
-    end
-    #opening output file
-    latt=zeros(Float64,m,7)
-    #computing lattice parameter
-    d=(V/m)^(1/3)
-    #iterate over all lattice state
-    m=1
-    for x=-(s+1):(s+1)
-        for y=-(s+1):(s+1)
-            for z=-(s+1):(s+1)
-                #positions in real dimensions
-                px=x
-                py=y
-                pz=z
-                #test if it is in the sphere
-                dis=sqrt(px^2+py^2+pz^2)
-                if dis<s
-                    #format: x,y,z,norm,real(alpha),imag(alpha): delimiter: tab
-                    latt[m,:]=[px*d,py*d,pz*d,dis*d,ref_id,d,d^3]
-                    m=m+1
+                if inside!=0
+                    append!(latt,[xc yc zc inside/total_sub 1])
                 end
             end
         end
     end
 
-    if verbose
-        println("lattice gegenerated with ", m-1, " dipoles")
-    end
-    return latt
+    latt=transpose(reshape(latt,(5,:)))
+    return latt, dx
+    #=
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.add_subplot(projection="3d")
+    n=length(latt[:,1])
+    color=zeros(n,4)
+    color[:,3].=1
+    color[:,4]=latt[:,4]
+    ax.voxels(latt[:,1],latt[:,2],latt[:,3],ones(n,n,n),facecolor=color,edgecolor="grey")
+    plt.show()
+    =#
 end
 
-@doc raw"""
-    gen_sphere_lattice_spheres(s,r;verbose=true)
-Discretizes a sphere of radius `R` in spheres on a cubic lattice where the radius of this sphere is divided in `s` lattice constants.
-`ref_id` is the refractive index id (we suppose that the entire sphere has the same refractive index.)
-Returns 2D array with first dimension is the `N` units that compose the sphere and the second is
-1) x-component of the position of the unit
-2) y-component of the position of the unit
-3) z-component of the position of the unit
-4) distance from the origin
-5) refractive index id
-6) lattice parameter
-7) volume of the spherical unit
-"""
-function gen_sphere_lattice_spheres(s,R;ref_id=1,verbose=true)
-    #logging
-    if verbose
-        println()
-        println("Generating a Sphere Lattice")
-        println()
-    end
-    #computing volume
-    V=(4/3)*pi*R^3
-    #printing
-    if verbose
-        println("you choose a sphere of radius ",R)
-        println("volume of the sphere:", V)
-        println("radius in units of lattice parameter=",s)
-    end
-    #counter
-    m=0
-    #iterate over all lattice state
-    for x=-(s+1):(s+1)
-        for y=-(s+1):(s+1)
-            for z=-(s+1):(s+1)
-                #positions in real dimensions
-                px=x
-                py=y
-                pz=z
-                #test if it is in the sphere
-                dis=sqrt(px^2+py^2+pz^2)
-                if dis<s
-                    #write if in the sphere
-                    m=m+1
-                end
-            end
-        end
-    end
-    if verbose
-        println("numberof dipoles: ",m)
-    end
-    #opening output file
-    latt=zeros(Float64,m,7)
-    #computing lattice parameter, diameter of the unit spheres
-    d=2*(3*V/m/4/pi)^(1/3)
-    #iterate over all lattice state
-    m=1
-    for x=-(s+1):(s+1)
-        for y=-(s+1):(s+1)
-            for z=-(s+1):(s+1)
-                #positions in real dimensions
-                px=x
-                py=y
-                pz=z
-                #test if it is in the sphere
-                dis=sqrt(px^2+py^2+pz^2)
-                if dis<s
-                    #format: x,y,z,norm,real(alpha),imag(alpha): delimiter: tab
-                    latt[m,:]=[px*d,py*d,pz*d,dis*d,ref_id,d,4/3*pi*d^3]
-                    m=m+1
-                end
-            end
-        end
-    end
 
-    if verbose
-        println("lattice gegenerated with ", m-1, " dipoles")
-    end
-    return latt
+
+function discretize_cube(L,N)
+     #lattice parameter
+     dx=L/N
+     #
+     latt=[]
+     #
+     for i=1:N
+         xc=(i-0.5)*dx-L/2
+         for j=1:N
+             yc=(j-0.5)*dx-L/2
+             for k=1:N
+                zc=(k-0.5)*dx-L/2
+                append!(latt,[xc yc zc 1])
+             end
+         end
+     end
+     latt=transpose(reshape(latt,(4,:)))
+     #=
+     fig = plt.figure(figsize=(12, 12))
+     ax = fig.add_subplot(projection="3d")
+     ax.scatter(latt[:,1],latt[:,2],latt[:,3])
+     plt.show()
+     =#
+     return latt,dx
 end
 end
