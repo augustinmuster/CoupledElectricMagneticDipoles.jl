47542c212d399c59ca3cbbcaa447b7c876b07250
augustinmuster <augustin@must-r.com>
2023-06-28 09:27:01 +0200

    adding diff. scattering cross section for e. dip.

--- src/processing.jl
+++ src/processing.jl
@@ -14,8 +14,11 @@ include("alpha.jl")
 @doc raw"""
     compute_dipole_moment(alpha,phi_inc)
 
-Computes the dipole moment (magnetic or electric) of a dipole with polarizability `alpha` under an incident field `phi_inc` 
-
+Computes the dipole moment (magnetic or electric) of a dipole with polarizability `alpha` under an incident field `phi_inc`. `alpha` can be:
+- a complex scalar
+- a 1D complex array of size ``N``
+- a ``3\times 3`` or ``6\times 6`` complex matrix.
+- a 3D complex array of size ``N\times 3\times 3`` or ``N\times 6\times 6``
 """
 function compute_dipole_moment(alpha,phi_inc)
     n=length(phi_inc[:,1])
@@ -45,7 +48,7 @@ end
     compute_cross_sections_e(knorm,kr,e_inc,alpha,input_field;explicit_scattering=true,verbose=true)
 
 Computes the extinction, absorbtion ans scattering cross section ``\sigma_{ext}``, ``\sigma_{abs}``, ``\sigma_{sca}`` of a system made out of electric dipoles.
-Notes that it sould follow the optical theorem (otherwise something is wrong), i.e.
+Notes that it sould follow the optical theorem, i.e.
 ```math
 \sigma_{ext}=\sigma_{abs}+\sigma_{sca}
 ```
@@ -53,8 +56,13 @@ Notes that it sould follow the optical theorem (otherwise something is wrong), i
 Inputs 
 - `knorm`: wavenumber
 - `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
-- `e_inc`: 2D complex array of size ``N\times 3`` containing the incident fields ``E_inc`` on every dipole.
-- `alpha`
+- `e_inc`: 2D complex array of size ``N\times 3`` containing the incident fields ``E_{inc}`` on every dipole.
+- `alpha_dl`: complex dimensionless electric polarisability of each dipoles. See the Alphas module for accepted formats.
+- `input_field`: 2D complex array of size ``N\times 3`` containing the input field ``E_0`` at each of the dipoles positions.
+- `explicit_scattering`: boolean that says wether to compute scttering cross section explicitely (formula) or to deduce it from the optical theorem. By default set to `true`.
+- `verbose`: whether to output pieces of information to the standard output during running or not. By default set to `true`.
+Outputs
+- an array containing in order: extinction, absorption and scattering cross section.
 """
 function compute_cross_sections_e(knorm,kr,e_inc,alpha_dl,input_field;explicit_scattering=true,verbose=true)
 
@@ -109,17 +117,33 @@ function compute_cross_sections_e(knorm,kr,e_inc,alpha_dl,input_field;explicit_s
     return [real(cext) real(cabs) real(csca)]
 end
 
-#*************************************************
-#COMPUTE THE POWERS FOR PLANE_WAVE INPUT FIELD AND ELECTRIC AND MAGNETIC DIPOLES
-#INPUTS:  norm of the wave vector, polarisations, incident fields, quasistatic polarisabilities,e0,whether to compute explicitely csca, verbose
-#OUTPUT: array with lambda, Cabs, Csca, Cext
-#*************************************************
-function compute_cross_sections_e_m(knorm,kr,phi_inc,alpha_e,alpha_m,input_field;explicit_scattering=true,verbose=true)
+@doc raw"""
+    compute_cross_sections_e_m(knorm,kr,phi_inc,alpha_e_dl,alpha_m_dl,input_field;explicit_scattering=true,verbose=true)
+
+Computes the extinction, absorbtion ans scattering cross section ``\sigma_{ext}``, ``\sigma_{abs}``, ``\sigma_{sca}`` of a system made out of electric dipoles.
+Notes that it sould follow the optical theorem, i.e.
+```math
+\sigma_{ext}=\sigma_{abs}+\sigma_{sca}
+```
+
+Inputs 
+- `knorm`: wavenumber
+- `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
+- `e_inc`: 2D complex array of size ``N\times 6`` containing the incident fields ``E_{inc}`` on every dipole.
+- `alpha_e_dl`: complex dimensionless electric polarisability of each dipoles. See the Alphas module for accepted formats.
+- `alpha_m_dl`: complex dimensionless magnetic polarisability of each dipoles. See the Alphas module for accepted formats.
+- `input_field`: input field on each dipole (``N\times 6``) array.
+- `explicit_scattering`: boolean that says wether to compute scttering cross section explicitely (formula) or to deduce it from the optical theorem. By default set to `true`.
+- `verbose`: whether to output pieces of information to the standard output during running or not. By default set to `true`.
+Outputs
+- an array containing in order: extinction, absorption and scattering cross section.
+"""
+function compute_cross_sections_e_m(knorm,kr,phi_inc,alpha_e_dl,alpha_m_dl,input_field;explicit_scattering=true,verbose=true)
     #redefine things
     e_inc=phi_inc[:,1:3]
     h_inc=phi_inc[:,4:6]
-    p=compute_dipole_moment(alpha_e,e_inc)
-    m=compute_dipole_moment(alpha_m,h_inc)
+    p=compute_dipole_moment(alpha_e_dl,e_inc)
+    m=compute_dipole_moment(alpha_m_dl,h_inc)
     e_inp=input_field[:,1:3]
     h_inp=input_field[:,4:6]
     if verbose
@@ -144,7 +168,7 @@ function compute_cross_sections_e_m(knorm,kr,phi_inc,alpha_e,alpha_m,input_field
         end
 
     end
-    cst=2*pi/knorm^2*2/dot(e_inp[1,1:3],e_inp[1,1:3])
+    cst=2*pi/knorm^2/dot(e_inp[1,1:3],e_inp[1,1:3])
     if (explicit_scattering)
         return [2*pi/knorm cst*sum_ext cst*sum_abs 2*cst*sum_sca]
     else
@@ -153,16 +177,83 @@ function compute_cross_sections_e_m(knorm,kr,phi_inc,alpha_e,alpha_m,input_field
 
 end
 
+@doc raw"""
+    poynting_vector(phi)
+Computes the poynting vector of a em field. Input is an electric and magnetic field `phi`(1D complex Array of length 6).
+Outputs a 1D float array of length 3.
+"""
+function poynting_vector(phi)
+    return 0.5*real(cross(phi[1:3],conj(phi[4:6])))
+end
 
 @doc raw"""
-    field_sca_e_m(kr, alpha_e_dl, alpha_m_dl, E_inc, krf)
-It computes the scattered field from the ensamble of dipoles.
+    diff_scattering_cross_section_e(knorm,kr,e_inc,alpha_e_dl,input_field,ur)
+
+Computes the differential scattering cross section of a system made out of electric dipoles in directions `ur`.
+
+Inputs 
+- `knorm`: wavenumber
+- `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
+- `e_inc`: 2D complex array of size ``N\times 3`` containing the incident fields ``E_{inc}`` on every dipole.
+- `alpha_dl`: complex dimensionless electric polarisability of each dipoles. See the Alphas module for accepted formats.
+- `input_field`: 2D complex array of size ``N\times 3`` containing the input field ``E_0`` at each of the dipoles positions.
+- `krf`: 2D float array of size ``Nf\times 3`` containing the dimentionless positions ``k\vec{r_f}`` where the scattered field is calculated.
+Outputs
+- an array containing the differential cross section in every directions.
+"""
+function diff_scattering_cross_section_e(knorm,kr,e_inc,alpha_e_dl,input_field,ur)
+    #if only one direction
+    if ndims(ur)==1
+        if norm(ur)!=1.
+            ur=ur/norm(ur)
+        end
+        n=length(kr[:,1])
+        max_norm=0
+        for i=1:n
+            if norm(kr[i,:])>max_norm
+                max_norm=norm(kr[i,:])
+            end
+        end
+        krf=knorm*ur*100*max_norm
+        
+        poynting=poynting_vector(far_field_sca_e(kr,e_inc,alpha_e_dl,krf))
+        d_sigma=(100*max_norm)^2*dot(poynting,ur)/dot(input_field[1,:],input_field[1,:])/2
+        return real(d_sigma)
+    #if more than one, i.e. 2D array
+    else
+        nur=length(ur[:,1])
+        for i=1:nur
+            if norm(ur[i,:])!=1.
+                ur[i,:]=ur[i,:]/norm(ur[i,:])
+            end
+        end
+        n=length(kr[:,1])
+        max_norm=0
+        for i=1:n
+            if norm(kr[i,:])>max_norm
+                max_norm=norm(kr[i,:])
+            end
+        end
+
+        d_sigma=zeros(nur)
+        for i=1:nur
+            krf=knorm*ur[i,:]*100*max_norm
+            poynting=poynting_vector(far_field_sca_e(kr,e_inc,alpha_e_dl,krf))
+            d_sigma[i]=(100*max_norm)^2*dot(poynting,ur[i,:])/dot(input_field[1,:],input_field[1,:])/2
+        end
+        return d_sigma
+    end
+end
+
+@doc raw"""
+    field_sca_e_m(kr, alpha_e_dl, alpha_m_dl, e_inc, krf)
+It computes the scattered field from the ensemble of dipoles.
 
 #Arguments
 - `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
-- `alpha_e_dl`: complex array containing the dimensionless electric polarisability.
-- `alpha_m_dl`: complex array containing the dimesnionless magnetic polarisability.
-- `E_inc`: 2D complex array of size ``N\times 6`` with the incident field in the dipoles.
+- `alpha_e_dl`: complex dimensionless electric polarisability of each dipoles. See the Alphas module for accepted formats.
+- `alpha_m_dl`: complex dimensionless magnetic polarisability of each dipoles. See the Alphas module for accepted formats.
+- `e_inc`: 2D complex array of size ``N\times 6`` with the incident field ``\phi_{inc}=(E_inc,H_inc)`` on the dipoles.
 - `krf`: 2D float array of size ``Nf\times 3`` containing the dimentionless positions ``k\vec{r_f}`` where the scattered field is calculated.
 
 #Outputs
@@ -182,7 +273,7 @@ Equation
 
 \E_{sca}(\r) = `field_r`
 """
-function field_sca_e_m(kr, alpha_e_dl, alpha_m_dl, E_inc, krf)
+function field_sca_e_m(kr, alpha_e_dl, alpha_m_dl, e_inc, krf)
 
     n_particles = length(kr[:,1]) 
     n_r0 = length(krf[:,1]) 
@@ -196,25 +287,25 @@ function field_sca_e_m(kr, alpha_e_dl, alpha_m_dl, E_inc, krf)
             G_tensor_fr[6 * (j-1) + 1:6 * (j-1) + 6 , 6 * (i-1) + 1:6 * (i-1) + 6] = [Ge*alp_e[i] im*Gm*alp_m[i]; -im*Gm*alp_e[i] Ge*alp_m[i]]
 	    end
     end
-    E_inc = reshape(transpose(E_inc),n_particles*6,)
-    field_r = G_tensor_fr*E_inc
+    e_inc = reshape(transpose(e_inc),n_particles*6,)
+    field_r = G_tensor_fr*e_inc
     return reshape(field_r,6,n_r0)   
 end
 
 @doc raw"""
-    field_sca_e_m(kr, alpha_dl, E_inc, krf)
-It computes the scattered field from the ensamble of dipoles.
+    field_sca_e_m(kr, alpha_dl, e_inc, krf)
+It computes the scattered field from the ensemble of dipoles.
 
 #Arguments
 - `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
-- `alpha_dl`: complex array containing the dimensionless polarisability.
-- `E_inc`: 2D complex array of size ``N\times 6`` with the incident field in the dipoles.
+- `alpha_dl`: complex dimensionless electric and magnetic polarisability of each dipoles. See the Alphas module for accepted formats.
+- `e_inc`: 2D complex array of size ``N\times 6`` with the incident field ``E_{inc}`` on the dipoles.
 - `krf`: 2D float array of size ``Nf\times 3`` containing the dimentionless positions ``k\vec{r_f}`` where the scattered field is calculated.
 
 #Outputs
 - `field_r`: 2D complex array of size ``Nf\times 6`` with the field scattered by the dipoles at every ``k\vec{r_f}``.
 """
-function field_sca_e_m(kr, alpha_dl, E_inc, krf)
+function field_sca_e_m(kr, alpha_dl, e_inc, krf)
 
     n_particles = length(kr[:,1]) 
     n_r0 = length(krf[:,1]) 
@@ -228,23 +319,23 @@ function field_sca_e_m(kr, alpha_dl, E_inc, krf)
             G_tensor_fr[6 * (j-1) + 1:6 * (j-1) + 6 , 6 * (i-1) + 1:6 * (i-1) + 6] = [Ge im*Gm; -im*Gm Ge]*alp[i]
 	    end
     end
-    E_inc = reshape(transpose(E_inc),n_particles*6,)
-    field_r = G_tensor_fr*E_inc
+    e_inc = reshape(transpose(e_inc),n_particles*6,)
+    field_r = G_tensor_fr*e_inc
     return reshape(field_r,6,n_r0)     
 end
 
 @doc raw"""
-    field_sca_e(kr, alpha_e_dl, alpha_m_dl, E_inc, krf)
-It computes the scattered field from the ensamble of dipoles.
+    function field_sca_e(kr, alpha_e_dl, e_inc, krf)
+It computes the scattered field from the ensemble of dipoles.
 
 #Arguments
 - `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
-- `alpha_e_dl`: complex array containing the dimensionless electric polarisability.
-- `E_inc`: 2D complex array of size ``N\times 6`` with the incident field in the dipoles.
+- `alpha_dl`: complex dimensionless electric polarisability of each dipoles. See the Alphas module for accepted formats.
+- `e_inc`: 2D complex array of size ``N\times 3`` with the incident field ``E_{inc}`` in the dipoles.
 - `krf`: 2D float array of size ``Nf\times 3`` containing the dimentionless positions ``k\vec{r_f}`` where the scattered field is calculated.
 
 #Outputs
-- `field_r`: 2D complex array of size ``Nf\times 6`` with the field scattered by the dipoles at every ``k\vec{r_f}``.
+- `field_r`: 2D complex array of size ``Nf\times 3`` with the field scattered by the dipoles at every ``k\vec{r_f}``.
 
 Equation
 
@@ -260,7 +351,7 @@ Equation
 
 \E_{sca}(\r) = `field_r`
 """
-function field_sca_e(kr, alpha_e_dl, E_inc, krf)
+function field_sca_e(kr, alpha_e_dl, e_inc, krf)
 
     n_particles = length(kr[:,1]) 
     n_r0 = length(krf[:,1]) 
@@ -274,9 +365,52 @@ function field_sca_e(kr, alpha_e_dl, E_inc, krf)
             G_tensor_fr[3 * (j-1) + 1:3 * (j-1) + 3 , 3 * (i-1) + 1:3 * (i-1) + 3] = Ge*alp_e[i] 
 	    end
     end
-    E_inc = reshape(transpose(E_inc),n_particles*3,)
-    field_r = G_tensor_fr*E_inc
+    e_inc = reshape(transpose(e_inc),n_particles*3,)
+    field_r = G_tensor_fr*e_inc
     return reshape(field_r,n_r0,3)      
+end
+
+@doc raw"""
+    function far_field_sca_e(kr,e_inc,alpha_e_dl,krf)
+It computes the scattered electric and magnetic field from the ensemble of dipoles in the far field approximation.
+
+# Arguments
+- `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
+- `alpha_dl`: complex dimensionless electric polarisability of each dipoles. See the Alphas module for accepted formats.
+- `e_inc`: 2D complex array of size ``N\times 3`` with the incident field ``E_{inc}`` in the dipoles.
+- `krf`: 2D float array of size ``Nf\times 3`` containing the dimentionless positions ``k\vec{r_f}`` where the scattered field is calculated.
+
+# Outputs
+- `field_r`: 2D complex array of size ``Nf\times 6`` with the electric and magnetic field scattered by the dipoles at every ``k\vec{r_f}``.
+
+"""
+function far_field_sca_e(kr,e_inc,alpha_e_dl,krf)
+    #number of dipoles
+    n=length(kr[:,1])
+    #dispatch alpha
+    alpha_e_dl=Alphas.dispatch_e(alpha_e_dl,n)
+    #if only one position
+    if ndims(krf)==1
+        res=zeros(ComplexF64,6)
+        for i=1:n
+            Ge,Gm=GreenTensors.G_em_far_field_renorm(krf,kr[i,:])
+            res[1:3]=res[1:3]+Ge*alpha_e_dl[i]*e_inc[i,:]
+            res[4:6]=res[4:6]-im*Gm*alpha_e_dl[i]*e_inc[i,:]
+        end
+        return res
+    #if more than 1, i.e. 2D array
+    else
+        nf=length(krf[i,:])
+        res=zeros(nf,6)
+        for j=1:nf
+            for i=1:n
+                Ge,Gm=GreenTesors.G_em_far_field_renorm(krf[j,:],kr[i,:])
+                res[j,1:3]=res[j,1:3]+Ge*alpha_e_dl[i]*e_inc[i,:]
+                res[j,4:6]=res[j,4:6]-im*Gm*alpha_e_dl[i]*e_inc[i,:]
+            end
+        end
+        return res
+    end
 end
 
 @doc raw"""
@@ -285,8 +419,8 @@ It Computes local density of states (LDOS) by the imaginary part of the returnin
 
 #Arguments
 - `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
-- `alpha_e_dl`: complex array containing the dimensionless electric polarisability.
-- `alpha_m_dl`: complex array containing the dimesnionless magnetic polarisability.
+- `alpha_e_dl`: complex dimensionless electric polarisability of each dipoles. See the Alphas module for accepted formats.
+- `alpha_m_dl`: complex dimensionless magnetic polarisability of each dipoles. See the Alphas module for accepted formats.
 - `Ainv`: (inverse) DDA matrix ``[I - G*alpha]^(-1)``.
 - `krd`: 2D float array of size ``Nd\times 3`` containing the dimentionless positions ``k\vec{r_d}`` where the LDOS is calculated.
 - `dip`: integer defining the dipole moment (``dip = 1`` is an electric x-dipole, ``dip = 2`` an elctric y-dipole...) or float array of size 6 with the desired dipole moment of the dipole.  
@@ -366,7 +500,7 @@ It Computes local density of states (LDOS) by the imaginary part of the returnin
 
 #Arguments
 - `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
-- `alpha_dl`: complex array containing the dimensionless polarisability.
+- `alpha_dl`: complex dimensionless electric and magnetic polarisability of each dipoles. See the Alphas module for accepted formats.
 - `Ainv`: (inverse) DDA matrix ``[I - G*alpha]^(-1)``.
 - `krd`: 2D float array of size ``Nd\times 3`` containing the dimentionless positions ``k\vec{r_d}`` where the LDOS is calculated.
 - `dip`: integer defining the dipole moment (``dip = 1`` is an electric x-dipole, ``dip = 2`` an elctric y-dipole...) or float array of size 6 with the desired dipole moment of the dipole.  
@@ -446,7 +580,7 @@ It Computes local density of states (LDOS) by the imaginary part of the returnin
 
 #Arguments
 - `kr`: 2D float array of size ``N\times 3`` containing the dimentionless positions ``k\vec{r}`` of each dipole.
-- `alpha_e_dl`: complex array containing the dimensionless electric polarisability.
+- `alpha_e_dl`: complex dimensionless electric polarisability of each dipoles. See the Alphas module for accepted formats.
 - `Ainv`: (inverse) DDA matrix ``[I - G*alpha]^(-1)``.
 - `krd`: 2D float array of size ``Nd\times 3`` containing the dimentionless positions ``k\vec{r_d}`` where the LDOS is calculated.
 - `dip`: integer defining the dipole moment (``dip = 1`` is an electric x-dipole, ``dip = 2`` an elctric y-dipole...) or float array of size 3 with the desired dipole moment of the dipole.  
