ac7516ec334c725733d01dcb857eb234e52818b8
augustinmuster <augustin@must-r.com>
2023-06-29 09:19:01 +0200

    upload PS sphere example

--- docs/src/miecoeff.md
+++ docs/src/miecoeff.md
@@ -1,1 +1,28 @@
  # Mie Coefficients
+
+ MieCoeff is a small modules to compute Mie Coefficients, mostly to use it to compute polarizabilities. It also contains some useful function to compute cross sections from the Mie theory. 
+ 
+Expressions are taken in Craig F. Bohren, Donald R. Huffman, *Absorption and Scattering of Light by Small Particles* (1998).
+
+ ## Functions Documentation
+
+
+```@docs
+CoupledElectricMagneticDipoles.MieCoeff.mie_an(vac_knorm, a, eps, eps_h,n)
+```
+
+```@docs
+CoupledElectricMagneticDipoles.MieCoeff.mie_bn(vac_knorm, a, eps, eps_h, n)
+```
+
+```@docs
+CoupledElectricMagneticDipoles.MieCoeff.mie_scattering_cross_section(knorm,a,eps,eps_h;cutoff=50)
+```
+
+```@docs
+CoupledElectricMagneticDipoles.MieCoeff.mie_extinction_cross_section(knorm,a,eps,eps_h;cutoff=50)
+```
+
+```@docs
+CoupledElectricMagneticDipoles.MieCoeff.mie_absorption_cross_section(knorm,a,eps,eps_h;cutoff=50)
+```
---
+++ example/example_PS_sphere/example_PS_sphere.jl
@@ -0,0 +1,120 @@
+#imports
+using PyCall
+using LaTeXStrings
+using Lebedev
+using LinearAlgebra
+@pyimport matplotlib.pyplot as plt
+@pyimport numpy as np
+include("../../src/DDA.jl")
+include("../../src/geometries.jl")
+include("../../src/processing.jl")
+include("../../src/alpha.jl")
+include("../../src/input_fields.jl")
+include("../../src/mie_coeff.jl")
+
+
+
+##################### Parameters ########################################
+#radius 
+a=250e-9
+#dielectric constant of the particle
+eps=(1.59)^2
+#dielectric constant of the medium
+eps_h=(1.33)^2
+#number of wavelengths to compute
+N_lambda=10
+lambda_min=1000e-9
+lambda_max=1100e-9
+#wavelengths to compute
+lambdas0=LinRange(1000e-9,1100e-9,N_lambda)
+lambdas=lambdas0/sqrt(eps_h)
+##########################################################################
+
+#discretizes a sphere in small cubes
+latt,dx=Geometries.discretize_sphere(a,10)
+
+#getting number of cubes in the discretized sphere
+n=length(latt[:,1])
+
+
+#createa an array to store results
+res=zeros(Float64,N_lambda,3)
+
+#=
+#solves DDA problem for each wavelength
+for i=1:N_lambda
+    #wavenumber in medium
+    knorm=2*pi/lambdas[i]
+    #computes polarizability for each dipoles using effective dielectric constant 
+    alpha=zeros(ComplexF64,n,3,3)
+    for j=1:n
+        eps_eff=latt[j,4]*eps+(1-latt[j,4])*eps_h
+        alpha[j,:,:]=Alphas.alpha_radiative(Alphas.alpha0_parallelepiped(dx,dx,dx,eps_eff,eps_h),knorm)
+    end
+    #computes input_field, a x-polarized plane-wave propagating along z
+    input_field=InputFields.plane_wave_e(knorm*latt[:,1:3])
+    #solves DDA
+    e_inc=DDACore.solve_DDA_e(knorm*latt[:,1:3],alpha,input_field=input_field,solver="CPU")
+    #computes cross section and save it in folder
+    res[i,1:end]=PostProcessing.compute_cross_sections_e(knorm,knorm*latt[:,1:3],e_inc,alpha,input_field;explicit_scattering=true,verbose=true)
+end
+
+
+#scattering cross section from the Mie theory
+res_mie=MieCoeff.mie_scattering_cross_section.(2 .*pi./lambdas,a,eps,eps_h;cutoff=50)
+
+#plotting the cross sections using matplotlib
+fig1,ax1=plt.subplots(2,sharex=true)
+#sets axis labels
+ax1[1].set_ylabel(L"Q_{sca}")
+ax1[2].set_ylabel(L"(Q_{ext}-Q_{abs}-Q_{abs})/Q_{ext}")
+ax1[2].set_xlabel(L"\lambda_0/a")
+#plot
+cst=pi*a^2
+ax1[1].plot(lambdas0./a,res[:,3]./cst,color="black",label="DDA, N="*string(n),marker="o")
+ax1[1].plot(lambdas0./a,res_mie./cst,color="red",label="Mie",marker="o")
+ax1[2].plot(lambdas0./a,(res[:,1].-res[:,2].-res[:,3])./res[:,1],color="black",marker="o")
+#legend and save
+ax1[1].legend()
+plt.tight_layout()
+fig1.savefig("Q_sca.svg")
+=#
+
+
+#computes polarizability for each dipoles using effective dielectric constant 
+knorm=2*pi/lambdas[1]
+alpha=zeros(ComplexF64,n,3,3)
+for j=1:n
+    eps_eff=latt[j,4]*eps+(1-latt[j,4])*eps_h
+    global  alpha[j,:,:]=Alphas.alpha_radiative(Alphas.alpha0_parallelepiped(dx,dx,dx,eps_eff,eps_h),knorm)
+end
+
+#computes input_field, a x-polarized plane-wave propagating along z
+input_field=InputFields.plane_wave_e(knorm*latt[:,1:3])
+#solves DDA
+e_inc=DDACore.solve_DDA_e(knorm*latt[:,1:3],alpha,input_field=input_field,solver="CPU")
+#computes cross section
+cs=PostProcessing.compute_cross_sections_e(knorm,knorm*latt[:,1:3],e_inc,alpha,input_field;explicit_scattering=true,verbose=true)
+
+
+#sampling direction an plotting 
+thetas=LinRange(0,2*pi,100)
+ur=zeros(100,3)
+ur[:,3]=cos.(thetas)
+ur[:,2]=sin.(thetas)
+
+#computes differential cross section
+res=PostProcessing.diff_scattering_cross_section_e(knorm,knorm*latt[:,1:3],e_inc,alpha,input_field,ur)
+
+#plotting
+fig2=plt.figure()
+ax2 = fig2.add_subplot(projection="polar")
+ax2.set_title(L"log(d Q_{sca}/ d \Omega)")
+ax2.plot(thetas,log10.(res/pi/(a^2)),label="y-z plane")
+plt.tight_layout()
+fig2.savefig("diff_Q_sca.svg")
+
+#Compare total scattering cross section and integral of the differential one
+x,y,z,w = lebedev_by_order(13)
+csca_int=4 * pi * dot(w,PostProcessing.diff_scattering_cross_section_e(knorm,knorm*latt[:,1:3],e_inc,alpha,input_field,[x y z]))
+println(cs[3]," : ",csca_int)
